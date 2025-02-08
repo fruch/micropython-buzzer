506d4225d4498958ff0fdc9adf2ed1a62e85f445
clach04 <clach04@gmail.com>
2025-02-08 15:07:17 -0800

    Working esp32 playback support #5
      * Faster board check when playing tone
      * Use minimum frequency when turning speaker/beeper/rumble off

--- buzzer/__init__.py
+++ buzzer/__init__.py
@@ -50,22 +50,36 @@ def isplit(iterable, sep=None):
     if r:
         yield r
 
+# TODO use an enum?
+PLATFORM_pyboard = 1
+PLATFORM_esp = 2
+
 class BuzzerPlayer(object):
 
-    def __init__(self, pin="X8", timer_id=1, channel_id=1, callback=None, platform=None):
+    def __init__(self, pin="X8", timer_id=1, channel_id=1, callback=None, platform=None, min_freq=None):
 
         if not platform:
             platform = sys.platform
-            
-        self.platform = platform
-        
-        if "esp" in platform:
-            from machine import PWM, Pin
-            self.buzzer_pin = PWM(Pin(pin, Pin.OUT), freq=1000)
 
-        elif platform == "pyboard":
+        if platform == "pyboard":
+            platform = PLATFORM_pyboard
+        elif "esp" in platform:  # esp8266 / esp32
+            platform = PLATFORM_esp
+        elif platform (PLATFORM_pyboard, PLATFORM_esp):
+            pass
+        else:
+            raise NotImplementedError('unsupported platform %r' % platform)
+
+        self.platform = platform
+
+        if platform == PLATFORM_esp:
+            from machine import PWM, Pin
+            self.min_freq = min_freq or 1_000  # ValueError: frequency must be from 1Hz to 40MHz
+            self.buzzer_pin = PWM(Pin(pin, Pin.OUT), freq=self.min_freq, duty=0)
+        elif platform == PLATFORM_pyboard:
             import pyb
             from pyb import Pin, Timer
+            self.min_freq = min_freq or 0
             self.pyb = pyb
             self.sound_pin = Pin(pin)
             self.timer = Timer(timer_id, freq=10000)
@@ -111,14 +125,14 @@ class BuzzerPlayer(object):
         self.play_tune(tempo, t, transpose=transpose, name=name)
 
     def tone(self, freq, duration=0, duty=30):
-        if self.platform == "esp8266":
+        if self.platform == PLATFORM_esp:
             self.buzzer_pin.freq(int(freq))
             self.buzzer_pin.duty(duty)
             time.sleep_us( int(duration * 0.9 * 1000) )
             self.buzzer_pin.duty(0)
             time.sleep_us(int(duration * 0.1 * 1000))
 
-        elif self.platform == "pyboard":
+        elif self.platform == PLATFORM_pyboard:
             self.timer.freq(freq)  # change frequency for change tone
             self.channel.pulse_width_percent(30)
             self.pyb.delay(duration)
@@ -136,14 +150,14 @@ class BuzzerPlayer(object):
             duration = int(full_note_in_samples / note_duration)
 
             if note_pitch == "r":
-                self.tone(0, duration, 0)
+                self.tone(self.min_freq, duration, 0)
             else:
                 freq = note_freq(note_pitch)
                 if transpose: freq *= 2 ** transpose
                 print("%s " % note_pitch, end="")
                 self.tone(freq, duration, 30)
-                
-        self.tone(0, 0, 0)
+
+        self.tone(self.min_freq, 0, 0)
 
     if MidiFile:
         def play_midi(self, filename, track=1,  transpose=6):
